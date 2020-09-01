7fc63bb1e53d7b242d4aced4e1dfbddd903cb501
ruubi0807 <h24081278@ncku.edu.tw>
2020-09-01 17:03:37 +0800

    資料庫連結

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Data
+from .models import Factcheck
 # Register your models here.
 class DataAdmin(admin.ModelAdmin):
 	data_display = ('title', 'pref')
 	
-admin.site.register(Data,DataAdmin)
+admin.site.register(Factcheck,DataAdmin)
