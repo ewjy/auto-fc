3538397f2a4457a315d9679894fe6cd3017f94e1
Ong Tsun-yau <ewtn053@gmail.com>
2020-09-01 11:03:17 +0800

    Updated django page links

--- main_app/views.py
+++ main_app/views.py
@@ -4,6 +4,5 @@ from django.contrib import admin
 # Create your views here.
 def index(request):
 	return render(request, "index.html")
-def about(request):
-	return render(request, "about.html")
-	
+def learn(request):
+    return render(request, "learn.html")
