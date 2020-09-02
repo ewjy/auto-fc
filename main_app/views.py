47f96623b115df14026ed3606d6c99e6b606b70c
ruubi0807 <h24081278@ncku.edu.tw>
2020-09-02 10:19:13 +0800

    搜尋功能

--- main_app/views.py
+++ main_app/views.py
@@ -3,13 +3,20 @@ from django.contrib import admin
 from .models import Factcheck
 # Create your views here.
 def index(request):
-	data = Factcheck.objects.all()
 	return render(request, "index.html")
 def learn(request):
     return render(request, "learn.html")
 def about(request):
     return render(request, "about.html")
 def results_title(request):
-	return render(request, "results_title.html")
+	query_list = Factcheck.objects.order_by('-title')
+	if 'query' in request.GET:
+		query = request.GET['query']
+		if query:
+			query_list = query_list.filter(title__icontains = query)
+	context = {
+		'result' : query_list
+	}
+	return render(request, "results_title.html",context)
 def results_url(request):
 	return render(request, "results_url.html")
